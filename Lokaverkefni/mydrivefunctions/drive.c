813fb215263c4c5c39bc549e7128e541e4d0cd85
AlexanderKari <alexanderra222@nemi.tskoli.is>
2017-12-11 12:35:44 +0000

    Forrita örlítið og læt nokkur verkefni inní forritið

--- Lokaverkefni/mydrivefunctions/drive.c
+++ Lokaverkefni/mydrivefunctions/drive.c
@@ -15,12 +15,12 @@ void remote(){
 		}
 		else
 		{
-			motor[clawMotor]= 0;
+		motor[clawMotor]= 0;
 		}
 
 		if(vexRT[Btn5U]==1)
 		{
-			motor[udMotor] = 127;
+		motor[udMotor] = 127;
 
 		}
 		else if(vexRT[Btn5D]== 1)
@@ -29,7 +29,7 @@ void remote(){
 		}
 		else
 		{
-			motor[udMotor]= 0;
+		motor[udMotor]= 0;
 		}
 	}
 
@@ -52,7 +52,7 @@ void forward()
 		motor[leftMotor]	= 63;
 	}
 
-	//Clear the encoders associated with the left and right motors
+  //Clear the encoders associated with the left and right motors
 	nMotorEncoder[rightMotor] = 0;
 	nMotorEncoder[leftMotor] = 0;
 
@@ -65,6 +65,7 @@ void forward()
 	}
 }
 
+const int BASETIME=925;
 
 void drive_backward(int i)
 {
@@ -78,4 +79,70 @@ void drive_forward(int i)
 	motor[rightMotor] = 127;
 	motor[leftMotor]  = 107;
 	wait1Msec(BASETIME*i);
+}
+void forward2()
+{
+
+	wait1Msec(200);
+			while(SensorValue(sonarSensor) = True)
+			{
+				motor[rightMotor] = 124;
+				motor[leftMotor]  = 124;
+				if(SensorValue(lightSensor) > 300)
+				{
+					motor[rightMotor] = 0;
+					motor[leftMotor]  = 0;
+					}
+				if (SensorValue(sonarSensor) < 69 || SensorValue(sonarSensor) == -1)
+				{
+					motor[rightMotor] = -123;
+					motor[leftMotor]  = 123;
+			}
+
+			}
+	}
+
+void verkefni5()
+{
+	 wait1Msec(200);
+
+  int threshold = 2500;
+  int threshold2 = 2700;
+
+  while(SensorValue[Button] < 1)
+  {
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+
+    displayLCDCenteredString(0, "LEFT  CNTR  RGHT");        //  Display   |
+    displayLCDPos(1,0);                                     //  Sensor    |
+    displayNextLCDNumber(SensorValue(lineFollowerLEFT));    //  Readings  |
+    displayLCDPos(1,6);                                     //  to LCD.   |
+    displayNextLCDNumber(SensorValue(lineFollowerCENTER));  //            |
+    displayLCDPos(1,12);                                    //  L  C  R   |
+    displayNextLCDNumber(SensorValue(lineFollowerRIGHT));   //  x  x  x   |
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+
+
+
+    if(SensorValue(lineFollowerRIGHT) > threshold)
+    {
+
+      motor[leftMotor]  = 80;
+      motor[rightMotor] = 0;
+    }
+
+    if(SensorValue(lineFollowerCENTER) > threshold2)
+    {
+
+      motor[leftMotor]  = 80;
+      motor[rightMotor] = 80;
+    }
+
+    if(SensorValue(lineFollowerLEFT) > threshold)
+    {
+
+      motor[leftMotor]  = 0;
+      motor[rightMotor] = 80;
+    }
+
+}
+
 }
--- Lokaverkefni/myheaders/myhead.h
+++ Lokaverkefni/myheaders/myhead.h
@@ -1,3 +1,2 @@
 #define BIGWEELBASE 234
-const int BASETIME=925;
 //const int SMALWEELBASE = 123;
